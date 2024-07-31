364f88d35285a03cff42727f47f9738f4b0b8df9
ajha417 <ajha417@rku.ac.in>
2024-07-31 09:05:59 +0530

    code changes to use dynamic movie info from movie db

--- src/main/java/com/javalearns/movieinfoservice/MovieInfoServiceApplication.java
+++ src/main/java/com/javalearns/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,9 +2,18 @@ package com.javalearns.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MovieInfoServiceApplication {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MovieInfoServiceApplication.class, args);
